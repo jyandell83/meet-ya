bf4a11ff93e84e1e644e7f7a3488370ba66ce53b
Jeremy Yandell <jereyand@gmail.com>
2019-12-28 16:51:23 -0800

    add route for rovers

--- server.js
+++ server.js
@@ -7,21 +7,25 @@ app.use(express.json({
     extended: false
 }));
 
-let view;
 
-axios.get('https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=2015-6-3&api_key=DEMO_KEY')
+
+
+app.get('/', async (req,res) => {
+    let viewOfImgs;
+    await axios.get('https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=2015-6-3&api_key=DEMO_KEY')
     .then(response => {
-        view = response.data.photos;
+        let view = response.data.photos;
+        viewOfImgs = view.map(photo => photo = photo.img_src);
     })
     .catch(error => {
     console.log(error);
+    });
+    res.send(viewOfImgs)
 });
 
-
-
-app.get('/', (req,res) => {
-    let viewOfImgs = view.map(photo => photo = photo.img_src)
-    res.send(viewOfImgs)
+app.get('/rovers', async (req,res) => {
+    let message = 'this is a page with all rovers'
+    res.send(message)
 }); 
 
 const PORT = 3000;
